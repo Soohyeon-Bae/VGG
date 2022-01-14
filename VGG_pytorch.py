4998c156c7e4e4a5a2604f133117a4309575238d
Soohyeon-Bae <94159857+Soohyeon-Bae@users.noreply.github.com>
2022-01-14 18:14:54 +0900

    Update VGG_pytorch.py

--- VGG_pytorch.py
+++ VGG_pytorch.py
@@ -1,86 +1,57 @@
+# keras로 구현한 것을 일대일로 torch 형식으로 바꾼 것.
+# conv 레이어를 일일이 적지 않아도 알맞는 파라미터가 적용되도록 할 수 있을 것 같은데 더 공부해봐야겠다. (22.01.14.)
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torchsummary import summary
 
-# VGG type dict
-# int : output chnnels after conv layer
-# 'M' : max pooling layer
-VGG_types = {
-    'VGG11' : [64, 'M', 128, 'M', 256, 256, 'M', 512,512, 'M',512,512,'M'],
-    'VGG13' : [64,64, 'M', 128, 128, 'M', 256, 256, 'M', 512,512, 'M', 512,512,'M'],
-    'VGG16' : [64,64, 'M', 128, 128, 'M', 256, 256,256, 'M', 512,512,512, 'M',512,512,512,'M'],
-    'VGG19' : [64,64, 'M', 128, 128, 'M', 256, 256,256,256, 'M', 512,512,512,512, 'M',512,512,512,512,'M']
-}
 
-# define VGGnet class
-class VGGnet(nn.Module):
-    def __init__(self, model, in_channels=3, num_classes=10, init_weights=True):
-        super(VGGnet, self).__init__()
-        self.in_channels = in_channels
+class VGG16(nn.Module):
+    def __init__(self):
+        super(VGG16, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, 3)
+        self.conv2 = nn.Conv2d(64, 64, 3)
+        self.conv3 = nn.Conv2d(64, 64, 3)
+        self.conv4 = nn.Conv2d(64, 128, 3)
+        self.conv5 = nn.Conv2d(128, 256, 3)
+        self.conv6 = nn.Conv2d(256, 256, 3)
+        self.conv7 = nn.Conv2d(256, 256, 3)
+        self.conv8 = nn.Conv2d(256, 512, 3)
+        self.conv9 = nn.Conv2d(512, 512, 3)
+        self.conv10 = nn.Conv2d(512, 512, 3)
+        self.conv11 = nn.Conv2d(512, 512, 3)
+        self.conv12 = nn.Conv2d(512, 512, 3)
+        self.conv13 = nn.Conv2d(512, 512, 3)
+        self.fc1 = nn.Linear(512*7*7, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, 1000)
 
-        # create conv_layers corresponding to VGG type
-        self.conv_layers = self.create_conv_laters(VGG_types[model])
-
-        self.fcs = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes),
-        )
-
-        # weight initialization
-        if init_weights:
-            self._initialize_weights()
+        self.pool = nn.MaxPool2d(2, 2)
 
     def forward(self, x):
-        x = self.conv_layers(x)
-        x = x.view(-1, 512 * 7 * 7)
-        x = self.fcs(x)
+
+        x = F.relu(self.conv1(x))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = self.pool(F.relu(self.conv4(x)))
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = self.pool(F.relu(self.conv7(x)))
+        x = F.relu(self.conv8(x))
+        x = F.relu(self.conv9(x))
+        x = self.pool(F.relu(self.conv10(x)))
+        x = F.relu(self.conv11(x))
+        x = F.relu(self.conv12(x))
+        x = self.pool(F.relu(self.conv13(x)))
+        x = torch.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.softmax(self.fc3(x))
+
         return x
 
-    # defint weight initialization function
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.constant_(m.bias, 0)
 
-    # define a function to create conv layer taken the key of VGG_type dict
-    def create_conv_laters(self, architecture):
-        layers = []
-        in_channels = self.in_channels  # 3
+model = VGG16().to("cuda:0")
+summary(model, input_size=(3, 224, 224))
 
-        for x in architecture:
-            if type(x) == int:  # int means conv layer
-                out_channels = x
-
-                layers += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
-                                     kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-                           nn.BatchNorm2d(x),
-                           nn.ReLU()]
-                in_channels = x
-            elif x == 'M':
-                layers += [nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))]
-
-        return nn.Sequential(*layers)
-
-
-# define device
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(device)
-
-# creat VGGnet object
-model = VGGnet('VGG16', in_channels=3, num_classes=10, init_weights=True).to(device)
-print(model)
-
-summary(model, input_size=(3, 224, 224), device=device.type)
